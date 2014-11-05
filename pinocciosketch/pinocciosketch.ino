cc24c2b2140abd82f05418e4291b5edfd5f71515
Anthony Bashi <bashia@uvic.ca>
2014-11-04 16:12:07 -0800

    Added basic sketch.

---
+++ pinocciosketch/pinocciosketch.ino
@@ -0,0 +1,15 @@
+#include <Pinoccio.h>
+#include <Scout.h>
+#include <ScoutHandler.h>
+#include <Shell.h>
+#include <SleepHandler.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
